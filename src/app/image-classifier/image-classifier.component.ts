5ddf68dbeccb189423766de2c2e21ce80cf2dc65
SkyLabCode <codenautas.app@gmail.com>
2025-04-10 18:16:16 +0200

    Update image-classifier.component.ts

--- src/app/image-classifier/image-classifier.component.ts
+++ src/app/image-classifier/image-classifier.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
 declare const tmImage: any;
 
 @Component({
@@ -33,10 +32,38 @@ export class ImageClassifierComponent {
     //3 Inicializamos la captura de la webcam
     await this.webcam.setup();
     await this.webcam.play();
-    await this.webcam.update();
+    //await this.webcam.update();
 
     //4 Insertamos la webcam (canvas) dentro de un div en html
     this.webcamContainer.nativeElement.appendChild(this.webcam.canvas);
+
+    //5 Establecemos el estado de la predicción como true
+    this.isPredicting = true;
+    this.loop();
+
+  }
+
+  //Función para disponer de un bucle continuo:
+  async loop(){
+
+    if(!this.isPredicting){
+      return;
+    }
+
+    await this.webcam.update();
+
+    const predictions = await this.model.predict(this.webcam.canvas);
+
+    //console.log(predictions)
+
+    //Extraemos la predicción con mayor probabilidad:
+    const max = predictions.sort((a: any, b: any) => b.probability - a.probability)[0];
+    //console.log(max);
+    this.prediction = `${max.className} - Percentage: ${(max.probability * 100).toFixed(2)}%`;
+    console.log(this.prediction)
+    //Llamamos a la funcionalidad nativa del navegador que se encarga
+    //de actualizar los frames de la captura de la webcam
+    window.requestAnimationFrame(() => this.loop());
 
   }
 
